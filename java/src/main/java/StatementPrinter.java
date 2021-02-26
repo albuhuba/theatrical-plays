2bc4a1bfe797955e0d70ddd87879eba96423fdbb
hua <huba.albu@siemens.com>
2021-02-26 11:26:51 +0100

    refactor volume credit

--- java/src/main/java/StatementPrinter.java
+++ java/src/main/java/StatementPrinter.java
@@ -4,6 +4,13 @@ import java.util.Map;
 
 public class StatementPrinter {
 
+    private class Tragedy{
+
+        private int baseAmount = 40000;
+        public Tragedy(){
+        }
+    }
+
     public String print(Invoice invoice, Map<String, Play> plays) {
         var totalAmount = 0;
         var volumeCredits = 0;
@@ -17,27 +24,41 @@ public class StatementPrinter {
 
             switch (play.type) {
                 case "tragedy":
+                    //new Tragedy().getAmount();
                     thisAmount = 40000 + getTragedyAmount(perf, 30, 1000 * (perf.audience - 30));
                     break;
                 case "comedy":
-                    thisAmount=30000 + getComedyAmount(perf);
+                    thisAmount = 30000 + getComedyAmount(perf);
                     break;
                 default:
                     throw new Error("unknown type: ${play.type}");
             }
 
-            // add volume credits
-            volumeCredits += Math.max(perf.audience - 30, 0);
-            // add extra credit for every ten comedy attendees
-            if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
-            // print line for this order
-            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+            volumeCredits += getVolumeCredits(perf, play);
             totalAmount += thisAmount;
+
+            result += String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience);
+
         }
         result += String.format("Amount owed is %s\n", frmt.format(totalAmount / 100));
         result += String.format("You earned %s credits\n", volumeCredits);
         return result;
+    }
+
+    private int getVolumeCredits(Performance perf, Play play) {
+        return addExtraVolumeCredits(perf, play);
+    }
+
+    private int addExtraVolumeCredits(Performance perf, Play play) {
+        if ("comedy".equals(play.type)){
+            return getBaseVolumeCredit(perf) + (int) Math.floor(perf.audience / 5);
+        }
+        return getBaseVolumeCredit(perf);
+    }
+
+    private int getBaseVolumeCredit(Performance perf) {
+        return Math.max(perf.audience - 30, 0);
     }
 
     private int getComedyAmount(Performance perf) {
