e63dae9714da48dd42c32dc67b08cc0f154f414f
hua <huba.albu@siemens.com>
2021-02-26 11:37:23 +0100

    introduce comedy class

--- java/src/main/java/StatementPrinter.java
+++ java/src/main/java/StatementPrinter.java
@@ -4,22 +4,44 @@ import java.util.Map;
 
 public class StatementPrinter {
 
-    private class Tragedy{
+    private class Tragedy {
 
         private static final int baseAmount = 40000;
         private final Performance performance;
 
-        public Tragedy(Performance performance){
+        public Tragedy(Performance performance) {
             this.performance = performance;
         }
 
         private int getAmount() {
             if (performance.audience > 30) {
-                return  baseAmount + 1000 * (performance.audience - 30);
+                return baseAmount + 1000 * (performance.audience - 30);
             }
             return baseAmount;
         }
+    }
 
+    private class Comedy {
+
+        private static final int baseAmount = 3000;
+        private final Performance performance;
+
+        public Comedy(Performance performance) {
+            this.performance = performance;
+        }
+
+        public int getAmount() {
+            return baseAmount + getComedyAmount(performance);
+        }
+
+        private int getComedyAmount(Performance perf) {
+            int thisAmount = 0;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            return thisAmount;
+        }
     }
 
     public String print(Invoice invoice, Map<String, Play> plays) {
@@ -38,7 +60,7 @@ public class StatementPrinter {
                     thisAmount = new Tragedy(perf).getAmount();
                     break;
                 case "comedy":
-                    thisAmount = 30000 + getComedyAmount(perf);
+                    thisAmount = new Comedy(perf).getAmount();
                     break;
                 default:
                     throw new Error("unknown type: ${play.type}");
@@ -61,7 +83,7 @@ public class StatementPrinter {
     }
 
     private int addExtraVolumeCredits(Performance perf, Play play) {
-        if ("comedy".equals(play.type)){
+        if ("comedy".equals(play.type)) {
             return getBaseVolumeCredit(perf) + (int) Math.floor(perf.audience / 5);
         }
         return getBaseVolumeCredit(perf);
@@ -69,15 +91,6 @@ public class StatementPrinter {
 
     private int getBaseVolumeCredit(Performance perf) {
         return Math.max(perf.audience - 30, 0);
-    }
-
-    private int getComedyAmount(Performance perf) {
-        int thisAmount = 0;
-        if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
-        }
-        thisAmount += 300 * perf.audience;
-        return thisAmount;
     }
 
 
